061d3b4c3255736cbc8e8ffb16eb5d6d92003edc
harsh082ip <109286547+harsh082ip@users.noreply.github.com>
2023-04-03 00:07:19 +0530

    Added a new file

---
+++ lib/main.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SafeArea(
+        child: Scaffold(
+          backgroundColor: Colors.blue,
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 60.0,
+                backgroundColor: Colors.amber,
+                backgroundImage: NetworkImage(
+                    'https://pbs.twimg.com/profile_images/1635029193913139200/lpZDxWWH_400x400.jpg'),
+              ),
+              Text(
+                "Harsh Vardhan",
+                style: TextStyle(
+                    fontFamily: 'Pacifico',
+                    fontSize: 40.0,
+                    color: Colors.white),
+              ),
+              Text(
+                'Flutter Developer',
+                style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  fontSize: 20.0,
+                  letterSpacing: 3.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.teal[100],
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.teal[100],
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    '+91 9555489012',
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    'lionrbl6@gmail.com',
+                    style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20.0),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+// Container(
+//                 margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+//                 padding: EdgeInsets.all(10.0),
+//                 color: Colors.white,
+//                 child: Row(
+//                   children: <Widget>[
+//                     Icon(
+//                       Icons.email,
+//                       color: Colors.teal,
+//                     ),
+//                     SizedBox(
+//                       width: 10.0,
+//                     ),
+//                     Text(
+//                       'harshhvstech1975@gmail.com',
+//                       style: TextStyle(
+//                         color: Colors.teal.shade900,
+//                         fontFamily: 'Source Sans Pro',
+//                         fontSize: 20.0,
+//                       ),
+//                     )
+//                   ],
+//                 ),
+//               ),
